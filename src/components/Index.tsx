003bb5ad913acf264155e45cb370374174180404
melrothiago <melrothiago@gmail.com>
2023-04-08 14:58:42 -0300

    css header

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,9 +1,19 @@
 import logo from '/src/assets/logo.svg'
+import styles from './Header.module.css'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 export function Header(){
     return (
-        <header>
-            <img src={logo} />  
+        <header className={styles.header}>
+            <img src={logo} />
+            <form className={styles.newTaskForm}>
+                <input placeholder='Adicione uma nova tarefa' />
+                <button>
+                    Criar
+                    <AiOutlinePlusCircle size={20} />
+                    </button>
+                
+            </form>  
         </header>
     )
 }
